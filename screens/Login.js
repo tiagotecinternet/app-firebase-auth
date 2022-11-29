655c85b3b7eda7e5fe58b3969fe2ef35bfd86da7
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-11-29 10:08:04 -0300

    Update Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,49 +1,64 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native'
+import { useState } from "react";
+import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    return (
-        <View style={estilos.container}>
-            <View style={estilos.formulario}>
-                <TextInput
-                    placeholder='E-mail'
-                    style={estilos.input}
-                />
-                <TextInput
-                    placeholder='Senha'
-                    style={estilos.input}
-                />
-                <View style={estilos.botoes}>
-                    <Button title='Entre' color="green" />
-                </View>
-            </View>
+  const login = () => {
+    if (!email || !senha) {
+      Alert.alert("Atenção!", "Você deve preencher todos os campos");
+      return; // parar o processo
+    }
+  };
+
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.formulario}>
+        <TextInput
+          onChangeText={(valor) => setEmail(valor)}
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+        />
+        <TextInput
+          onChangeText={(valor) => setSenha(valor)}
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+        />
+
+        <View style={estilos.botoes}>
+          <Button title="Entre" color="green" onPress={login} />
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default Login
+export default Login;
 
 const estilos = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'lightgreen',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    formulario: {
-        marginBottom: 32,
-        width: "80%"
-    },
-    input: {
-        backgroundColor: "white",
-        marginVertical: 8,
-        padding: 8,
-        borderRadius: 4
-    },
-    botoes: {
-        marginVertical: 8,
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "lightgreen",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formulario: {
+    marginBottom: 32,
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 4,
+  },
+  botoes: {
+    marginVertical: 8,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
