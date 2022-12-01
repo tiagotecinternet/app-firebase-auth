441d3baecbaf1842bc28c301bf16f939b4f2b4a2
tiagoprogwebt22 <tiago.tecinternet22@gmail.com>
2022-12-01 09:06:27 -0300

    Update Cadastro.js

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -1,50 +1,65 @@
-import { Button, StyleSheet, TextInput, View } from 'react-native'
+import { useState } from "react";
+import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+
+import { auth } from "../firebaseConfig";
 
 const Cadastro = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-    return (
-        <View style={estilos.container}>
-            <View style={estilos.formulario}>
-                <TextInput
-                    placeholder='E-mail'
-                    style={estilos.input}
-                />
-                <TextInput
-                    placeholder='Senha'
-                    style={estilos.input}
-                />
-                <View style={estilos.botoes}>
-                    <Button title='Cadastre-se' color="blue" />
-                </View>
+  const cadastrar = () => {
+    if (!email || !senha) {
+      Alert.alert("Atenção", "Você deve preencher e-mail e senha");
+      return;
+    }
+  };
 
-            </View>
+  return (
+    <View style={estilos.container}>
+      <View style={estilos.formulario}>
+        <TextInput
+          placeholder="E-mail"
+          style={estilos.input}
+          keyboardType="email-address"
+          onChangeText={(valor) => setEmail(valor)}
+        />
+        <TextInput
+          placeholder="Senha"
+          style={estilos.input}
+          secureTextEntry
+          onChangeText={(valor) => setSenha(valor)}
+        />
+        <View style={estilos.botoes}>
+          <Button onPress={cadastrar} title="Cadastre-se" color="blue" />
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default Cadastro
+export default Cadastro;
 
 const estilos = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'lightblue',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    formulario: {
-        marginVertical: 16,
-        width: "80%"
-    },
-    input: {
-        backgroundColor: "white",
-        marginVertical: 8,
-        padding: 8,
-        borderRadius: 4
-    },
-    botoes: {
-        marginVertical: 8,
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between"
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  formulario: {
+    marginVertical: 16,
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    marginVertical: 8,
+    padding: 8,
+    borderRadius: 4,
+  },
+  botoes: {
+    marginVertical: 8,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
